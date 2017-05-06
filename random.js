9609a656fb23384a19b7f60ae56b5a527d145cc1
Doubleshot1122 <skibo1122@yahoo.com>
2017-05-06 14:04:21 -0700

    single contact is now all current contacts and delete is working for each

--- random.js
+++ random.js
@@ -16,3 +16,26 @@ results.forEach(element => {
 
 
 <p><%= results[0].first_name + " " + results[0].last_name%></p>
+
+
+<%results.forEach(element => {%>
+<div class="row">
+  <div class="col-md-6">
+    <h3><%= element.first_name + " " + element.last_name%></h3>
+    <p>Phone: <%= element.phone_number%></p>
+    <p>Email: <%= element.email_address%></p>
+    <p><%= element.line_1 + " " + element.line_2%></p>
+    <p><%= element.city +  ", " + element.state + " " + element.zip%></p>
+    <img src="<%= element.image_url%>" alt="">
+    <div class="row"><br></div>
+    <form action="/contacts/<%= element.cid%>?_method=DELETE" method="POST">
+      <input
+        class="btn btn-sm btn-danger"
+        type="submit"
+        name="delete"
+        value="DELETE">
+    </form>
+    <hr>
+  </div>
+</div><!-- row end  -->
+<%})%>
