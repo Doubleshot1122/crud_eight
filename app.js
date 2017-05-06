1af150a0c6a944b77ac41974156a288dea2a2c5c
Doubleshot1122 <skibo1122@yahoo.com>
2017-05-06 10:57:13 -0700

    Added method-override to package.json and added contacts route info

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var engine = require('ejs-locals');
+var override = require('method-override');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var contacts = require('./routes/contacts');
 
 var app = express();
 
 // view engine setup
+app.engine('ejs', engine)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -23,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/contacts', contacts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
